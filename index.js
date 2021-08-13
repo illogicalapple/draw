3fd6bfb22ece677d8ff77e287a7fabce12dad141
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2021-08-13 18:27:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ addEventListener("load", function() {
 	const context = canvas.getContext("2d");
 	canvas.addEventListener("mousedown", function() { mouseDown = true });
 	canvas.addEventListener("mouseup", function() { mouseDown = false });
+	canvas.width = canvas.getBoundingClientRect().width;
+	canvas.height = canvas.getBoundingClientRect().height;
+	addEventListener("resize", function() {
+		canvas.width = canvas.getBoundingClientRect().width;
+		canvas.height = canvas.getBoundingClientRect().height;
+	});
 	context.strokeStyle = "black";
 	context.lineCap = "round";
 	context.lineWidth = 10;
