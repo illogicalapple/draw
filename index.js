b8dff60817ae5c6a7e3acfef1b8c6ca8ed104e54
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2021-10-10 11:21:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+var drawing = [];
+const DOWN = "DOWN";
+const UP = "UP";
+var redraw;
 addEventListener("load", function() {
 	const canvas = document.querySelector("canvas#draw");
 	const context = canvas.getContext("2d");
 	var x, y, mouseDown;
-	function mouseDownEvent(event) { [x, y, mouseDown] = [event.clientX - 17.5, event.clientY - 92.5, true] };
-	function mouseUpEvent() { mouseDown = false };
+	function mouseDownEvent(event) { [x, y, mouseDown] = [event.clientX - 17.5, event.clientY - 92.5, true]; drawing.push([x, y]); drawing.push(DOWN); };
+	function mouseUpEvent() { mouseDown = false; drawing.push(UP); };
 	function mouseMoveEvent(event) {
 		if(mouseDown) {
 			context.strokeStyle = "black";
@@ -14,8 +18,33 @@ addEventListener("load", function() {
 			[x, y] = [event.clientX - 17.5, event.clientY - 92.5];
 			context.lineTo(x, y);
 			context.stroke();
+			drawing.push([x, y]);
 		}
 	};
+	redraw = function redraw() {
+		context.strokeStyle = "black";
+		context.lineCap = "round";
+		context.lineWidth = 5;
+		mouseDown = false;
+		canvas.width += 0; //clear
+		for(let element of drawing) {
+			if(element === DOWN) {
+				mouseDown = true;
+			} else if(element === UP) {
+				mouseDown = false;
+			} else {
+				if(mouseDown) {
+					context.beginPath();
+					context.moveTo(x, y);
+					[x, y] = element;
+					context.lineTo(x, y);
+					context.stroke();
+				} else {
+					[x, y] = element;
+				}
+			}
+		}
+	}
 	document.addEventListener("mousedown", mouseDownEvent);
 	document.addEventListener("mouseup", mouseUpEvent);
 	document.addEventListener("touchstart", mouseDownEvent);
@@ -27,5 +56,6 @@ addEventListener("load", function() {
 	addEventListener("resize", function() {
 		canvas.width = canvas.getBoundingClientRect().width;
 		canvas.height = canvas.getBoundingClientRect().height;
+		drawing = [];
 	});
 });
